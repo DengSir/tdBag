6bca294dc59432de30d1f1086fa3c13121e810a6
DengSir <tdaddon@163.com>
2018-08-23 00:42:26 +0800

    feat: add BankFix.lua

---
+++ BankFix.lua
@@ -0,0 +1,33 @@
+-- BankFix.lua
+-- @Author : DengSir (tdaddon@163.com)
+-- @Link   : https://dengsir.github.io
+-- @Date   : 8/22/2018, 11:45:59 PM
+
+local ADDON, Addon = ...
+
+local BankFix = Addon:NewModule('BankFix', 'AceEvent-3.0')
+
+local AutoDisplay = Addon:GetModule('AutoDisplay')
+
+function BankFix:OnInitialize()
+    self.BankHiddenParent = CreateFrame('Frame')
+    self.BankHiddenParent:Hide()
+end
+
+function BankFix:OnEnable()
+    self:RegisterEvent('BANKFRAME_OPENED')
+    self:RegisterEvent('BANKFRAME_CLOSED')
+
+    HideUIPanel(BankFrame)
+    BankFrame:SetParent(self.BankHiddenParent)
+    BankFrame:ClearAllPoints()
+    BankFrame:SetPoint('TOPLEFT', 0, 0)
+end
+
+function BankFix:BANKFRAME_OPENED()
+    BankFrame:Show()
+end
+
+function BankFix:BANKFRAME_CLOSED()
+    BankFrame:Hide()
+end
